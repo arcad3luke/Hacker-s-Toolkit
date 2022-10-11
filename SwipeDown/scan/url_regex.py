4bdd6e74c246d9dd795acea848b4ab848aa607de
arcad3luke <51435427+arcad3luke@users.noreply.github.com>
2022-10-10 22:36:00 -0400

    amends

--- SwipeDown/scan/url_regex.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def urlRegex():
-    url_decode = re.search('/[A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+/g')
-    with open('url_decoded_regex.txt', 'w') as f:
-        decoded_file = url_decode.split('')
-        f.close(decoded_file)
-    
--- SwipeDown/scan/urlscrape.py
+++ SwipeDown/scan/urlscrape.py
@@ -32,6 +32,7 @@ count = 1
 
 for i in matches:
     file.write(str(count) + '.' + i + '\n')
+    file.write(re.search('/[A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+/g').split('\n'))
     count += 1
 
 file.write("The were " + str(len_match) + " matches found for your keyword:\n")

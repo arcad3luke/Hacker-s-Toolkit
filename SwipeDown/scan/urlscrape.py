7f7fe0f75dad33e1f0b22e01af9eec5245a35721
arcad3luke <51435427+arcad3luke@users.noreply.github.com>
2022-11-03 20:59:34 -0400

    Toying with re.findall.

--- SwipeDown/scan/urlscrape.py
+++ SwipeDown/scan/urlscrape.py
@@ -21,7 +21,7 @@ def scrape(count=1):
     while count <= 100:
         for i in matches:
             file.write(str(count) + '.' + i + '\n')
-            file.write(re.search('/[A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+/g').split('\n'))
+            file.write(re.findall('/[A-Za-z]+:[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;.=]+/g',i,flags=re.DOTALL))
             file.write("The were " + str(len_match) + " matches found for your keyword:\n")
             file.close()
             count += 1

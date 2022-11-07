0b75767055e15ce50e2872a66d45097890a7ea84
arcad3luke <51435427+arcad3luke@users.noreply.github.com>
2022-11-07 11:52:45 -0500

    Storing the queries is not a problem now, but the duplication is annoying.

--- SwipeDown/scan/google_scrape.py
+++ SwipeDown/scan/google_scrape.py
@@ -1,155 +1,28 @@
 import requests
 import bs4
 
-KEYWORDS = "admin | index of '/admin' | cpanel | admin login | admin.php"
+query = "allinurl: index of '/admin'"
 
-# HTML tags
-'''htmltags = '<a>,' \
-           '<br>,' \
-           '<hr>,' \
-           '<h1>,' \
-           '<h2>,' \
-           '<h3>,' \
-           '<h4>,' \
-           '<h5>,' \
-           '<h6>,' \
-           '<strong>,' \
-           '<em>,' \
-           '<abbr>,' \
-           '<acronym>,' \
-           '<address>,' \
-           '<bdo>,' \
-           '<blockquote>,' \
-           '<cite>,' \
-           '<q>,' \
-           '<code>,' \
-           '<ins>,' \
-           '<del>,' \
-           '<dfn>,' \
-           '<kbd>,' \
-           '<pre>,' \
-           '<samp>,' \
-           '<var>,' \
-           '<base>,' \
-           '<img>,' \
-           '<area>,' \
-           '<map>,' \
-           '<param>,' \
-           '<object>,' \
-           '<ul>,' \
-           '<ol>,' \
-           '<li>,' \
-           '<dl>,' \
-           '<dt>,' \
-           '<dd>,' \
-           '<table>,' \
-           '<tr>,' \
-           '<td>,' \
-           '<th>,' \
-           '<tbody>,' \
-           '<thead>,' \
-           '<tfoot>,' \
-           '<col>,' \
-           '<colgroup>,' \
-           '<caption>,' \
-           '<form>,' \
-           '<input>,' \
-           '<textarea>,' \
-           '<select>,' \
-           '<option>,' \
-           '<optgroup>,' \
-           '<button>,' \
-           '<label>,' \
-           '<fieldset>,' \
-           '<legend>,' \
-           '<script>,' \
-           '<noscript>,' \
-           '<!-->,' \
-           '<!DOCTYPE>,' \
-           '<applet>,' \
-           '<article>,' \
-           '<aside>,' \
-           '<audio>,' \
-           '<b>,' \
-           '<basefont>,' \
-           '<bdi>,' \
-           '<big>,' \
-           '<body>,' \
-           '<canvas>,' \
-           '<center>,' \
-           '<cite>,' \
-           '<data>,' \
-           '<datalist>,' \
-           '<details>,' \
-           '<dialog>,' \
-           '<dir>,' \
-           '<div>,' \
-           '<embed>,' \
-           '<fieldset>,' \
-           '<figcaption>,' \
-           '<figure>,' \
-           '<font>,' \
-           '<footer>,' \
-           '<form>,' \
-           '<frame>,' \
-           '<frameset>,' \
-           '<head>,' \
-           '<header>,' \
-           '<html>,' \
-           '<i>,' \
-           '<iframe>,' \
-           '<ins>,' \
-           '<isindex>,' \
-           '<link>,' \
-           '<main>,' \
-           '<map>,' \
-           '<mark>,' \
-           '<marquee>,' \
-           '<menu>,' \
-           '<meta>,' \
-           '<meter>,' \
-           '<nav>,' \
-           '<noframes>,' \
-           '<optgroup>,' \
-           '<output>,' \
-           '<picture>,' \
-           '<pre>,' \
-           '<progress>,' \
-           '<rp>,' \
-           '<rt>,' \
-           '<ruby>,' \
-           '<s>,' \
-           '<section>,' \
-           '<small>,' \
-           '<source>,' \
-           '<span>,' \
-           '<strike>,' \
-           '<strong>,' \
-           '<style>,' \
-           '<sub>,' \
-           '<summary>,' \
-           '<sup>,' \
-           '<svg>,' \
-           '<template>,' \
-           '<time>,' \
-           '<title>,' \
-           '<track>,' \
-           '<video>,' \
-           '<wbr>'
-'''
-
-url = f'https://www.google.com/search?q={KEYWORDS}'
+url = 'https://www.google.com/search?q=' + query
 result = requests.get(url)
 
 soup = bs4.BeautifulSoup(result.text, 'html.parser')
 
 tags = soup.find_all()
+search = soup.find("div", class_='BNeawe').text
 
-print(soup)
+# print(soup)
 
-for tag in tags:
-    print(tag.getText())
-    print('---------')
+count = 0
 
-with open() as f:
-    f.writelines()
+while count <= 2:
+    for x in soup.find_all(limit=1):
+        if len(x.getText(strip=True)) == 0:
+            x.extract()
+        for tag in tags:
+            print(tag.getText().split('\n'))
+            print('---------')
+        count += 1
+        with open('scraping.txt','a+') as f:
+            f.writelines(search)
+            f.close()

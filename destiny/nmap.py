d21796100380d9e44e8acda97e6ff6531bbda23d
destiny creates <40739698+destiny-creates@users.noreply.github.com>
2022-11-07 21:18:33 -0500

    redoing the nmap file to allow more flexibility from the user, and allow more input.

--- destiny/nmap.py
+++ destiny/nmap.py
@@ -1,8 +1,68 @@
 import nmap3
 import json
 
+nmap = nmap3.Nmap()
 
-def nmapscanner():
+
+def menu():
+    print("""          _______  _        _______  _______  _______  _______ 
+|\     /|(  ____ \( \      (  ____ \(  ___  )(       )(  ____ \
+| )   ( || (    \/| (      | (    \/| (   ) || () () || (    \/
+| | _ | || (__    | |      | |      | |   | || || || || (__    
+| |( )| ||  __)   | |      | |      | |   | || |(_)| ||  __)   
+| || || || (      | |      | |      | |   | || |   | || (      
+| () () || (____/\| (____/\| (____/\| (___) || )   ( || (____/\
+(_______)(_______/(_______/(_______/(_______)|/     \|(_______/""")
+    choose = input("""Which tool would you like to use?
+
+[1] OS Detection
+[2] DNS scan
+[3] Version detection
+[4] Scan top ports
+[5] Scan all
+
+Tool:""")
+    if choose == "1":
+        nmapos()
+    elif choose == "2":
+        nmapdns()
+    elif choose == "3":
+        nmapversion()
+    elif choose == "4":
+        nmapports()
+    elif choose == "5":
+        nmapall()
+
+
+def nmapos():
+    target = input("\nTarget: ")
+    print(f"\nScanning {target}, please wait...")
+    os_results = nmap.nmap_os_detection(target)
+    print(json.dumps(os_results, indent=4, sort_keys=True))
+
+
+def nmapdns():
+    target = input("Target: ")
+    print(f"\nScanning {target}, please wait...")
+    dns_results = nmap.nmap_dns_brute_script(target)
+    print(json.dumps(dns_results, indent=4, sort_keys=True))
+
+
+def nmapversion():
+    target = input("Target: ")
+    print(f"\nScanning {target}, please wait...")
+    version_results = nmap.nmap_version_detection(target)
+    print(json.dumps(version_results, indent=4, sort_keys=True))
+
+
+def nmapports():
+    target = input("Target: ")
+    print(f"\nScanning {target}, please wait...")
+    ports = nmap.scan_top_ports(target)
+    print(json.dumps(ports, indent=4, sort_keys=True))
+
+
+def nmapall():
     nmap = nmap3.Nmap()
     target = input("Target: ")
     print("\nScanning, please wait: " + target)
@@ -21,5 +81,4 @@ def nmapscanner():
     print(json.dumps(ports, indent=4, sort_keys=True))
 
 
-nmapscanner()
-    
+menu()

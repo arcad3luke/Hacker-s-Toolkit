fb3d9dd09dd87c842fb2b8181fbeb5ba4ca2cd30
arcad3luke <51435427+arcad3luke@users.noreply.github.com>
2022-11-18 22:41:33 -0500

    Added reference script for shortcuts in the editor and any other tips and tricks in both code and editor.

---
+++ SwipeDown/utility/Sandbox/reference.py
@@ -0,0 +1,6 @@
+print('''All this was just a test of a pyhcarm feature:
+Ctrl+Alt+Shift+C creates a reference to a line of code within a file.
+I thought that this would copy/paste the line itself, but this simply
+puts a reference to the line number associated with the code.
+SwipeDown/SwipeDown/Utility/shell_server.py:9\n''')
+
